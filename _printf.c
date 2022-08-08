e2470e5c7077fe936268658991315e10390139ce
Precious Diala <dialpressy@gmail.com>
2022-08-08 18:06:43 +0000

    Updated the _printf file

--- _printf.c
+++ _printf.c
@@ -37,7 +37,7 @@ int _printf(const char *format, ...)
 			pfunc = get_print(*p);
 			count += (pfunc)
 				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
+				 _printf("%%%c", *p);
 		} else
 			count += _putchar(*p);
 	}
