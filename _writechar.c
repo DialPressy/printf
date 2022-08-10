5c637dfca7ff0fec9e2ec19e4572a3b86faae6d3
Precious Diala <dialpressy@gmail.com>
2022-08-10 08:07:33 +0000

    Update

--- _writechar.c
+++ _writechar.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - writes the character c to stdout.
+ * @c: The character to print.
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * Description: _putchar uses a local buffer of 1024 to call write
- * as little as possible
+ * as little as possible.
  */
 int _putchar(char c)
 {
