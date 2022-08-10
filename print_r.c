8142c4732c268a7fae9b9591ab3bec730c56787f
Bodiseowei Tonweriyai <bmtonweriyai@gmail.com>
2022-08-10 03:19:40 -0700

    update print_r.c file

--- print_r.c
+++ print_r.c
@@ -94,7 +94,7 @@ int print_rot13(va_list l, flags_t *f)
 }
 
 /**
- * print_percent - prints a percent
+ * print_percent - the function prints a percent
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags in which we turn the flags on
  * Return: number of char printed
