2eb3c9b760c38a256fe76ee0116f1235fd4207ae
Precious Diala <dialpressy@gmail.com>
2022-08-10 07:27:24 +0000

    Update

--- main.h
+++ main.h
@@ -30,7 +30,7 @@ typedef struct printHandler
 	int (*f)(va_list ap, flags_t *f);
 } ph;
 
-/* print_nums */
+/* print_i */
 int print_int(va_list l, flags_t *f);
 void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
@@ -58,11 +58,11 @@ int get_flag(char s, flags_t *f);
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
 
-/* write_funcs */
+/* _write_char */
 int _putchar(char c);
 int _puts(char *str);
 
-/* print_custom */
+/* print_r */
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
